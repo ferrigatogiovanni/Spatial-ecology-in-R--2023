3c9a6a20502b9db14a80da9c80334102accf7f63
ferrigatogiovanni <146854328+ferrigatogiovanni@users.noreply.github.com>
2023-10-12 17:57:27 +0200

    Update 03.populations.distribution.R

--- 03.populations.distribution.R
+++ 03.populations.distribution.R
@@ -9,10 +9,10 @@ file <- system.file("external/species.shp", package="sdm")
 file #returns the location of the file (species)
 
 # looking at the set
-species <- vect(file)
+species <- vect(file) #in this file there are vectors 
 
 # looking at the occurrences
-specie$Occurence # how to see the presences (1 present, 0 absent)
+specie$Occurence # how to see the presences of the specie (1 present, 0 absent)
 
 # plot
 plot(species)
@@ -30,12 +30,58 @@ plot(species[species$Occurrence == 1,],col='blue',pch=16) #selects the species t
 plot(species[species$Occurrence == 0,],col='red',pch=16) #selects the species that are absent
 
 # putting two plots near each others
-par(mfrow <- c(1,2))
+par(mfrow = c(1,2))
 plot(pres)
 plot(abse)
 
 # it erases the previous plots/graphics
 dev.off()
 
+# plotting presences and absences altogether
+plot(pres)
+points(abse, col = "blue")
 
+# how to check the path
+path <- system.file("external", package="sdm") 
 
+# predictors: environmental variables
+# elevation predictor 
+elev <- system.file("external/elevation.asc", package="sdm")
+elev
+elevmap <- rast(elev)    #from Terra package (rast are images)
+plot(elevmap)
+points(pres, cex = .5) #adding the presences 
+
+# temperature predictor
+temp <- system.file("external/temperature.asc", package="sdm")
+temp #it shows the path to the file temperature
+tempmap <- rast(temp)
+plot(tempmap)
+points(pres, cex = .5)
+
+# vegetation predictor
+vege <- system.file("external/vegetation.asc", package="sdm")
+vegemap <- rast(vege)
+plot(vegemap)
+points(pres, cex = .5)
+
+# precipitation predictor
+prec <- system.file("external/precipitation.asc", package="sdm")
+precmap <- rast(prec)
+plot(precmap)
+points(pres, cex = .5)
+
+# final multiframe 
+par(mfrow = c(2,2))
+
+plot(elevmap)
+points(pres, cex = .5)
+
+plot(tempmap)
+points(pres, cex = .5)
+
+plot(vegemap)
+points(pres, cex = .5)
+
+plot(precmap)
+points(pres, cex = .5)
