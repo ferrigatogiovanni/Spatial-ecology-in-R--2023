11a36d30fc1fda8be5d82cd3b9472fb5ad59604f
ferrigatogiovanni <146854328+ferrigatogiovanni@users.noreply.github.com>
2023-12-07 16:36:43 +0100

    Update 09.classification.R

--- 09.classification.R
+++ 09.classification.R
@@ -2,3 +2,11 @@
 #you can calculate the distances between the reflectances and assign a class (e.g. water,urban area, mountain) to each pixel based on the reflactance 
 
 # Classifying satellite images and estimates the amount of change
+
+library(terra)
+library(imageRy)
+
+im.list()
+
+#importing the sun image
+sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
