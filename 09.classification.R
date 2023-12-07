4e099a4a6a33aaa861305574107f3b1efd667a47
ferrigatogiovanni <146854328+ferrigatogiovanni@users.noreply.github.com>
2023-12-07 16:50:25 +0100

    Update 09.classification.R

--- 09.classification.R
+++ 09.classification.R
@@ -8,5 +8,15 @@ library(imageRy)
 
 im.list()
 
-#importing the sun image
+#importing the sun image from imageRy
 sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+#classifying the image sun just imported
+sunc <- im.classify(sun, num_clusters = 3) 
+
+plot(sunc)
+#how can I know which class has the highest energy?
+plot(sun) (yellow corresponds to higher energy and black corresponds to low energy)
+plot(sunc)
+#just plot the two and see where the colour overlaps; in my case class 2 (yellow) corresponds to the higher energy and class 3 (green) correrponds to the lower energy 
+(class 1 - white - corresponds to an intermediate level of energy)
