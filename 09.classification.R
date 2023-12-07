ca967d859172f15df0b365155209119be87d7da1
ferrigatogiovanni <146854328+ferrigatogiovanni@users.noreply.github.com>
2023-12-07 16:32:01 +0100

    Create 09.classification.R

---
+++ 09.classification.R
@@ -0,0 +1,4 @@
+#each pixel have its own level of refluectance
+#you can calculate the distances between the reflectances and assign a class (e.g. water,urban area, mountain) to each pixel based on the reflactance 
+
+# Classifying satellite images and estimates the amount of change
