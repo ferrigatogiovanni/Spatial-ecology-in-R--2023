4f7f498d109b5323b920176f2a19efef89180c07
ferrigatogiovanni <146854328+ferrigatogiovanni@users.noreply.github.com>
2023-12-08 10:23:52 +0100

    Update 03.communities.multivar.R

--- 03.communities.multivar.R
+++ 03.communities.multivar.R
@@ -15,15 +15,17 @@ ldc2 <- 3.1166
 ldc3 <- 1.30055 
 ldc4 <- 1.47888
 
+#total length
 total = ldc1 + ldc2 + ldc3 + ldc4
 
+#calculating the percentage on the total for each ldc
 pldc1 = ldc1 * 100 / total
 pldc2 = ldc2 * 100 / total
 pldc3 = ldc3 * 100 / total
 pldc4 = ldc4 * 100 / total
 
 pldc1; pldc2 
-pldc1 + pldc2 
+pldc1 + pldc2 #summing the first 2 to find the lenth of the axis
 
 plot(ord)
 #see bromus hordeaceus (Bromhord in the plot)
