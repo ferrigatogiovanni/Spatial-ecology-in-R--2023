3a24a06cc4a38e1bd813d7bd943054ef0a1bf45c
ferrigatogiovanni <146854328+ferrigatogiovanni@users.noreply.github.com>
2024-01-11 17:32:20 +0100

    Update colorblind.R

--- colorblind.R
+++ colorblind.R
@@ -1,3 +1,10 @@
 # simulating colour blind vision 
 
 library(devtools)
+library(colorblindr)
+library(ggplot2)
+
+fig <- ggplot(iris, aes(Sepal.Length, fill = Species)) + geom_density(alpha = 0.7)
+fig
+
+cvd_grid(fig)
