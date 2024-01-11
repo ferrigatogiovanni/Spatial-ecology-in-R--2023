f9946940cfc3e263a8ece697f3d75537e430f706
ferrigatogiovanni <146854328+ferrigatogiovanni@users.noreply.github.com>
2024-01-11 17:42:09 +0100

    Update colorblind.R

--- colorblind.R
+++ colorblind.R
@@ -4,7 +4,17 @@ library(devtools)
 library(colorblindr)
 library(ggplot2)
 
+iris
+head(iris)
+
 fig <- ggplot(iris, aes(Sepal.Length, fill = Species)) + geom_density(alpha = 0.7)
 fig
 
+# vision by different colour blind people
 cvd_grid(fig)
+
+names(iris)
+# doing the same for Sepal.Width
+fig2 <- ggplot(iris, aes(Sepal.Width, fill = Species)) + geom_density(alpha = 0.7)
+fig2
+cvd_grid(fig2)
